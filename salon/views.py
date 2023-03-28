ca6d9de5467bf25a88a211a0f2f5cdf7d3ab3c2d
Volodymyr47 <volodymyr.di@gmail.com>
2023-03-28 19:00:22 +0300

    Specialists select changed. ORDER_BY to services added

--- salon/views.py
+++ salon/views.py
@@ -4,20 +4,22 @@ from datetime import datetime, timedelta
 
 
 TIMEDELTA = timedelta(days=7)
+CURRENT_TIME = datetime.today()
+REQUIRED_PERIOD = CURRENT_TIME + TIMEDELTA
+
 
 def home(request):
     return render(request, 'salon/home.html')
 
 
 def services(request):
-    current_time = datetime.today()
-    required_period = current_time + TIMEDELTA
+
     all_services = {}
 
     try:
         all_services = Service.objects.filter(specialist__status=2,
-                                              specialist__workschedule__end_time__range=(current_time, required_period)
-                                              ).distinct()
+                                              specialist__workschedule__end_time__range=(CURRENT_TIME, REQUIRED_PERIOD)
+                                              ).distinct().order_by('name')
     except Exception as err:
         print(err)
 
@@ -33,7 +35,12 @@ def service(request, service_name):
         service_details = Service.objects.filter(name=service_name).all()
         for detail in service_details:
             service_id = detail.id
-        available_specialists = Specialist.objects.filter(services__id=service_id)
+        available_specialists = \
+            Specialist.objects.filter(status=2,
+                                      services__id=service_id,
+                                      workschedule__end_time__range=(CURRENT_TIME, REQUIRED_PERIOD)
+                                      ).distinct().order_by('name')
+        print(available_specialists.query)
     except Exception as err:
         print(err)
 
